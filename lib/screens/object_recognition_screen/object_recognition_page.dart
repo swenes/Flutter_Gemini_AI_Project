52af8624193ee78ee40354cec385280810aa3687
Enes <enes_aydogdu2001@hotmail.com>
2024-03-15 16:16:45 +0300

    edited directories

---
+++ lib/screens/object_recognition_screen/object_recognition_page.dart
@@ -0,0 +1,109 @@
+import 'package:camera/camera.dart';
+import 'package:err_detector_project/controller/scan_controller.dart';
+import 'package:err_detector_project/utils/constants.dart';
+import 'package:err_detector_project/widgets/border_container.dart';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OcrPage extends StatelessWidget {
+  const OcrPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: GetBuilder<ScanController>(
+          init: ScanController(),
+          builder: (controller) {
+            return controller.isCameraInitialized.value
+                ? SafeArea(
+                    child: Column(
+                      children: [
+                        Stack(
+                          children: [
+                            CameraPreview(controller.cameraController),
+                            Positioned(
+                              top: controller.y != null
+                                  ? controller.y * 650
+                                  : 100,
+                              right: controller.x != null
+                                  ? controller.x * 550
+                                  : 100,
+                              child: Container(
+                                width: controller.w != null
+                                    ? (controller.w * 100 * context.width / 100)
+                                    : 100,
+                                height:
+                                    controller.h * 100 * context.height / 100,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  border: Border.all(
+                                      color: Colors.green, width: 4.0),
+                                ),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.all(8),
+                                      color: Colors.white.withOpacity(0.5),
+                                      child: Text(controller.label),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        // Expanded ekleyerek, Container'ın geri kalan yüksekliği almasını sağladık
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            child: Container(
+                              width: context.width - 80,
+                              decoration: BoxDecoration(
+                                  color: const Color.fromARGB(255, 131, 4, 4),
+                                  border:
+                                      Border.all(color: Constants.borderColor),
+                                  borderRadius: BorderRadius.circular(40)),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      'Şu an Tespit Edilen Nesne',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Cera Pro'),
+                                    ),
+                                    Text(
+                                      controller.label,
+                                      style: const TextStyle(
+                                          color:
+                                              Color.fromARGB(255, 34, 226, 66),
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Cera Pro'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : const Center(
+                    child: BorderContainer(
+                        color: Colors.white,
+                        text: "Kameranıza Erişiyoruz..",
+                        fontSize: 20));
+          },
+        ));
+  }
+}
---
+++ lib/screens/speech_to_text_screen/speech_to_text_page.dart
@@ -0,0 +1,182 @@
+// import 'package:err_detector_project/screens/speech_to_text_screen/save_notes_page.dart';
+// import 'package:flutter/material.dart';
+// //import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+// class Note {
+//   String text;
+//   DateTime timestamp;
+
+//   Note(this.text) : timestamp = DateTime.now();
+// }
+
+// class SpeechToTextPage extends StatefulWidget {
+//   const SpeechToTextPage({super.key});
+
+//   @override
+//   // ignore: library_private_types_in_public_api
+//   _SpeechToTextPageState createState() => _SpeechToTextPageState();
+// }
+
+// class _SpeechToTextPageState extends State<SpeechToTextPage> {
+//   TextEditingController noteNameController = TextEditingController();
+//   late stt.SpeechToText _speech;
+//   bool _isListening = false;
+//   String _text = '';
+//   // ignore: prefer_final_fields
+//   List<Note> _notes = [];
+//   bool _isFirstWord = true;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _speech = stt.SpeechToText();
+//   }
+
+//   void _listen() async {
+//     if (!_isListening) {
+//       bool available = await _speech.initialize(
+//         onStatus: (status) {
+//           debugPrint('onStatus: $status');
+//         },
+//         onError: (errorNotification) {
+//           debugPrint('onError: $errorNotification');
+//         },
+//       );
+
+//       if (available) {
+//         setState(() {
+//           _isListening = true;
+//           _text = 'Şuan seni duyabiliyorum..';
+//           _isFirstWord = true;
+//         });
+
+//         _speech.listen(
+//           onResult: (result) {
+//             setState(() {
+//               if (_isFirstWord) {
+//                 _text = result.recognizedWords;
+//                 _isFirstWord = false;
+//               } else {
+//                 _text += ' ${result.recognizedWords}';
+//               }
+//             });
+//           },
+//         );
+//       }
+//     } else {
+//       setState(() {
+//         _isListening = false;
+//         _text = '';
+//       });
+//       _speech.stop();
+//     }
+//   }
+
+//   void _showSaveNoteDialog() async {
+//     await showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: const Text('Notu Kaydet'),
+//           content: Column(
+//             children: [
+//               TextField(
+//                 controller: noteNameController,
+//                 decoration: const InputDecoration(labelText: 'Not Adı'),
+//               ),
+//             ],
+//           ),
+//           actions: [
+//             ElevatedButton(
+//               onPressed: () {
+//                 if (noteNameController.text.isNotEmpty) {
+//                   _saveNote(noteNameController.text);
+//                   Navigator.pop(context);
+//                 }
+//               },
+//               child: const Text('Kaydet'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+
+//   void _saveNote(String noteName) {
+//     setState(() {
+//       _notes.add(Note(noteNameController.text));
+//     });
+//     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+//       content: Text('Not kaydedildi: $noteName'),
+//     ));
+//   }
+
+//   void _navigateToSavedNotesPage() {
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) => SavedNotesPage(notes: _notes),
+//       ),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: const Text('Hızlı Not Al',
+//             style:
+//                 TextStyle(fontFamily: 'Cera-Pro', fontWeight: FontWeight.w500)),
+//         actions: [
+//           IconButton(
+//             icon: const Icon(Icons.save),
+//             onPressed: _showSaveNoteDialog,
+//           ),
+//           IconButton(
+//             icon: const Icon(Icons.list),
+//             onPressed: _navigateToSavedNotesPage,
+//           ),
+//         ],
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: [
+//           if (!_isListening && _text.isEmpty)
+//             Container(
+//               margin: const EdgeInsets.all(30),
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(20),
+//                 border: Border.all(width: 0.4, color: Colors.blueGrey),
+//               ),
+//               padding: const EdgeInsets.all(16.0),
+//               child: const Text(
+//                 'Sizi duyamadım. Sanırım henüz bir şey söylemediniz.',
+//                 style: TextStyle(fontSize: 18.0),
+//                 textAlign: TextAlign.center,
+//               ),
+//             ),
+//           AnimatedContainer(
+//             duration: const Duration(milliseconds: 3000),
+//             padding: const EdgeInsets.all(16.0),
+//             alignment: Alignment.center, // İçeriği ortalamak için
+//             child: Text(
+//               _text,
+//               style: const TextStyle(fontSize: 18.0),
+//               textAlign: TextAlign.center,
+//             ),
+//           ),
+//           const SizedBox(height: 20),
+//           IconButton(
+//             icon: Icon(
+//               _isListening ? Icons.stop : Icons.mic,
+//               size: 30,
+//             ),
+//             onPressed: _listen,
+//             tooltip: 'Dinlemeye Başla/Durdur',
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
